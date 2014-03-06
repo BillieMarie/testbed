131e7efe3cdeee78290136f9827f2eecf32c0e1d
BillieMarie <billie_marie@yahoo.com>
2014-03-06 12:58:36 -0800

    Disclaimer
    A simple disclaimer in JS form

---
+++ disclaimer.js
@@ -0,0 +1,33 @@
+/*
+	Written by Billie Marie Matteo
+	April 9, 2013. All Rights Reserved
+	Version 1.0
+*/
+
+var yourCompany = ("The-Web-Pros.com"); //Your Website Name Here
+
+document.write("<h1>" + yourCompany + " Disclaimer</h1>");
+document.write("<h3>1. Acceptance of our Terms</h3>");
+document.write("<p> By visiting the website " + yourCompany + ", viewing, accessing or   otherwise using any of the services or information created, collected,   compiled or submitted to " + yourCompany + ", you agree to be bound by the   following Terms and Conditions of Service. If you do not want to be   bound by our Terms your only option is not to visit, view or otherwise   use the services of " + yourCompany + ". You understand, agree and   acknowledge that these Terms constitute a legally binding agreement   between you and " + yourCompany + " and that your use of " + yourCompany + " shall indicate your conclusive acceptance of this agreement. </p>");
+document.write("<h3>2. Provision of Services</h3>");
+document.write("<p> You agree and acknowledge that " + yourCompany + " is entitled to modify,   improve or discontinue any of its services at its sole discretion and   without notice to you even if it may result in you being prevented from   accessing any information contained in it. Furthermore, you agree and acknowledge that " + yourCompany + " is entitled to provide services to you through subsidiaries or affiliated entities. </p>");
+document.write("<h3>3. Proprietary Rights</h3>");
+document.write("<p> You acknowledge and agree that " + yourCompany + " may contain proprietary and confidential information including trademarks, service marks and patents protected by intellectual property laws and international intellectual property treaties. " + yourCompany + " authorizes you to view   and make a single copy of portions of its content for offline, personal, non-commercial use. Our content may not be sold, reproduced, or distributed without our written permission. Any third-party trademarks, service marks and logos are the property of their respective   owners. Any further rights not specifically granted herein are reserved. </p>");
+document.write("<h3>4. Submitted Content</h3>");
+document.write("<p> When you submit content to " + yourCompany + " you simultaneously grant   " + yourCompany + " an irrevocable, worldwide, royalty free license to publish, display, modify, distribute and syndicate your content worldwide. You confirm and warrant that you have the required authority to grant the above license to " + yourCompany + ". </p>");
+document.write("<h3>5. Termination of Agreement</h3>");
+document.write("<p> The Terms of this agreement will continue to apply in perpetuity until terminated by either party without notice at any time for any reason. Terms that are to continue in perpetuity shall be unaffected by the termination of this agreement. </p>");
+document.write("<h3>6. Disclaimer of Warranties</h3>");
+document.write("<p> You understand and agree that your use of " + yourCompany + " is entirely at your own risk and that our services are provided \"As Is\" and \"As   Available\". " + yourCompany + " does not make any express or implied   warranties, endorsements or representations whatsoever as to the   operation of the " + yourCompany + " website, information, content,   materials, or products. This shall include, but not be limited to, implied warranties of merchantability and fitness for a particular purpose and non-infringement, and warranties that access to or use of the service will be uninterrupted or error-free or that defects in the service will be corrected. </p>");
+document.write("<h3>7. Limitation of Liability</h3>");
+document.write("<p> You understand and agree that " + yourCompany + " and any of its   subsidiaries or affiliates shall in no event be liable for any direct,   indirect, incidental, consequential, or exemplary damages. This shall   include, but not be limited to damages for loss of profits, business   interruption, business reputation or goodwill, loss of programs or information or other intangible loss arising out of the use of or the inability to use the service, or information, or any permanent or   temporary cessation of such service or access to information, or the deletion or corruption of any content or information, or the failure to store any content or information. The above limitation shall apply whether or not " + yourCompany + " has been advised of or should have been   aware of the possibility of such damages. In jurisdictions where the   exclusion or limitation of liability for consequential or incidental   damages is not allowed the liability of " + yourCompany + " is limited to the greatest extent permitted by law. </p>");
+document.write("<h3>8. External Content</h3>");
+document.write("<p> " + yourCompany + " may include hyperlinks to third-party content,   advertising or websites. You acknowledge and agree that " + yourCompany + " is not responsible for and does not endorse any advertising, products or resource available from such resources or websites. </p>");
+document.write("<h3>9. Jurisdiction</h3>");
+document.write("<p> You expressly understand and agree to submit to the personal and   exclusive jurisdiction of the courts of the country, state, province or territory determined solely by " + yourCompany + " to resolve any legal matter arising from this agreement or related to your use of " + yourCompany + ". If the court of law having jurisdiction, rules that any provision of the agreement is invalid, then that provision will be removed from the Terms and the remaining Terms will continue to be valid. </p>");
+document.write("<h3>10. Entire Agreement</h3>");
+document.write("<p> You understand and agree that the above Terms constitute the entire   general agreement between you and " + yourCompany + ". You may be subject to additional Terms and conditions when you use, purchase or access other services, affiliate services or third-party content or material. </p>");
+document.write("<h3>11. Changes to the Terms</h3>");
+document.write("<p> " + yourCompany + " reserves the right to modify these Terms from time to time at our sole discretion and without any notice. Changes to our Terms become effective on the date they are posted and your continued use of " + yourCompany + " after any changes to Terms will signify your agreement to be bound by them.</p>");
+
+
